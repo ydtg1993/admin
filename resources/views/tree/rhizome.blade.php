8e8e7e2b0ca9e4d2ce56b120ea0d584219d2b553
ydtg1993 <946818508@qq.com>
2019-02-10 15:37:51 +0800

    *

--- resources/views/tree/rhizome.blade.php
+++ resources/views/tree/rhizome.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
     <div>
-        <div class="uk-card uk-card-default uk-card-hover uk-card-body" style="overflow-x: scroll">
-            <div style="display: block;width: 2000px">
+        <div class="uk-card uk-card-default uk-card-hover uk-card-body" style="overflow-x: scroll;overflow-y: scroll">
+            <div style="display: block;width: max-content;height: 600px">
             {!! $tree_view !!}
             </div>
         </div>
